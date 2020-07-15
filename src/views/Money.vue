3ded344d84061ed4d0739207ccf2404a01f2bd5f
liukuai <937613925@qq.com>
2020-07-15 16:45:14 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>money
+    <div class="nav-wrapper">
+        <div class="content">
+            money
+        </div>
+        <Nav/>
     </div>
 </template>
 
@@ -8,5 +12,15 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .nav-wrapper{
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        border: 1px solid green;
+    }
+    .content{
+        border: 1px solid blue;
+        overflow: auto;
+        flex-grow: 1;
+    }
 </style>
