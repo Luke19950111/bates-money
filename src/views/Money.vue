0c9d096781f3d1b3f1eecb0d50b4176774f88001
liukuai <937613925@qq.com>
2020-07-17 16:42:07 +0800

    Money css布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,28 +1,5 @@
 <template>
-    <Layout>
-        <div class="tags">
-            <ul class="current">
-                <li>衣</li>
-                <li>食</li>
-                <li>住</li>
-                <li>行</li>
-            </ul>
-            <div class="new">
-                <button>新增标签</button>
-            </div>
-        </div>
-        <div>
-            <label class="notes">
-                <span class="name">备注</span>
-                <input type="text" placeholder="在这里输入备注">
-            </label>
-        </div>
-        <div>
-            <ul class="types">
-                <li class="selected">支出</li>
-                <li>收入</li>
-            </ul>
-        </div>
+    <Layout class-prefix="layout">
         <div class="numberPad">
             <div class="output">100</div>
             <div class="buttons">
@@ -42,6 +19,29 @@
                 <button>.</button>
             </div>
         </div>
+        <div>
+            <ul class="types">
+                <li class="selected">支出</li>
+                <li>收入</li>
+            </ul>
+        </div>
+        <div>
+            <label class="notes">
+                <span class="name">备注</span>
+                <input type="text" placeholder="在这里输入备注">
+            </label>
+        </div>
+        <div class="tags">
+            <div class="new">
+                <button>新增标签</button>
+            </div>
+            <ul class="current">
+                <li>衣</li>
+                <li>食</li>
+                <li>住</li>
+                <li>行</li>
+            </ul>
+        </div>
     </Layout>
 </template>
 
@@ -50,15 +50,27 @@
         name: 'Money'
     };
 </script>
-
+<style lang="scss">
+    .layout-content {
+        display: flex;
+        flex-direction: column-reverse;
+    }
+</style>
 <style lang="scss" scoped>
     @import "~@/assets/style/helper.scss";
-    .tags{
+
+    .tags {
         font-size: 14px;
         padding: 16px;
-        > .current{
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column-reverse;
+
+        > .current {
             display: flex;
-            > li{
+            flex-wrap: wrap;
+
+            > li {
                 background: #d9d9d9;
                 $h: 24px;
                 height: $h;
@@ -66,11 +78,14 @@
                 border-radius: $h/2;
                 padding: 0 16px;
                 margin-right: 12px;
+                margin-top: 4px;
             }
         }
-        > .new{
+
+        > .new {
             padding-top: 16px;
-            button{
+
+            button {
                 background: transparent;
                 border: none;
                 color: #999;
@@ -79,16 +94,19 @@
             }
         }
     }
-    .notes{
+
+    .notes {
         background: #f5f5f5;
         font-size: 14px;
         padding-left: 16px;
         display: flex;
         align-items: center;
-        .name{
+
+        .name {
             padding-right: 16px;
         }
-        input{
+
+        input {
             height: 64px;
             flex-grow: 1;
             background: transparent;
@@ -96,20 +114,23 @@
             padding-right: 16px;
         }
     }
-    .types{
+
+    .types {
         background: #c4c4c4;
         display: flex;
         text-align: center;
         font-size: 24px;
-        > li{
+
+        > li {
             width: 50%;
             height: 64px;
             display: flex;
             justify-content: center;
             align-items: center;
             position: relative;
-            &.selected{
-                &::after{
+
+            &.selected {
+                &::after {
                     content: '';
                     position: absolute;
                     bottom: 0;
@@ -121,48 +142,62 @@
             }
         }
     }
-    .numberPad{
-        .output{
+
+    .numberPad {
+        .output {
+            @extend %innerShadow;
             font-size: 36px;
             font-family: Consolas, monospace;
             padding: 9px 16px;
             text-align: right;
         }
-        .buttons{
+
+        .buttons {
             @extend %clearFix;
-            > button{
+
+            > button {
                 width: 25%;
                 height: 64px;
                 float: left;
                 background: transparent;
                 border: none;
-                &.ok{
+
+                &.ok {
                     height: 64*2px;
                     float: right;
                 }
-                &.zero{
+
+                &.zero {
                     width: 25*2%;
                 }
+
                 $bg: #f2f2f2;
-                &:nth-child(1){
+
+                &:nth-child(1) {
                     background: $bg;
                 }
-                &:nth-child(2), &:nth-child(5){
+
+                &:nth-child(2), &:nth-child(5) {
                     background: darken($bg, 4%);
                 }
-                &:nth-child(3), &:nth-child(6), &:nth-child(9){
+
+                &:nth-child(3), &:nth-child(6), &:nth-child(9) {
                     background: darken($bg, 4*2%);
                 }
-                &:nth-child(4), &:nth-child(7), &:nth-child(10){
+
+                &:nth-child(4), &:nth-child(7), &:nth-child(10) {
                     background: darken($bg, 4*3%);
                 }
-                &:nth-child(8), &:nth-child(11), &:nth-child(13){
+
+                &:nth-child(8), &:nth-child(11), &:nth-child(13) {
                     background: darken($bg, 4*4%);
                 }
-                &:nth-child(14){
+
+                &:nth-child(14) {
                     background: darken($bg, 4*5%);
                 }
-                &:nth-child(12){
+
+                &:nth-child(12) {
                     background: darken($bg, 4*6%);
                 }
             }
