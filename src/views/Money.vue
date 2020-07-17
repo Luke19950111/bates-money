c608d5738ea5db40bf524c433181aff2268433d3
liukuai <937613925@qq.com>
2020-07-17 15:59:04 +0800

    Money numberPad样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -24,8 +24,8 @@
             </ul>
         </div>
         <div class="numberPad">
-            <div class="outPut">100</div>
-            <div class="numbers">
+            <div class="output">100</div>
+            <div class="buttons">
                 <button>1</button>
                 <button>2</button>
                 <button>3</button>
@@ -37,8 +37,8 @@
                 <button>7</button>
                 <button>8</button>
                 <button>9</button>
-                <button>OK</button>
-                <button>0</button>
+                <button class="ok">OK</button>
+                <button class="zero">0</button>
                 <button>.</button>
             </div>
         </div>
@@ -52,6 +52,7 @@
 </script>
 
 <style lang="scss" scoped>
+    @import "~@/assets/style/helper.scss";
     .tags{
         font-size: 14px;
         padding: 16px;
@@ -116,6 +117,53 @@
                     width: 100%;
                     height: 4px;
                     background: #333;
+                }
+            }
+        }
+    }
+    .numberPad{
+        .output{
+            font-size: 36px;
+            font-family: Consolas, monospace;
+            padding: 9px 16px;
+            text-align: right;
+        }
+        .buttons{
+            @extend %clearFix;
+            > button{
+                width: 25%;
+                height: 64px;
+                float: left;
+                background: transparent;
+                border: none;
+                &.ok{
+                    height: 64*2px;
+                    float: right;
+                }
+                &.zero{
+                    width: 25*2%;
+                }
+                $bg: #f2f2f2;
+                &:nth-child(1){
+                    background: $bg;
+                }
+                &:nth-child(2), &:nth-child(5){
+                    background: darken($bg, 4%);
+                }
+                &:nth-child(3), &:nth-child(6), &:nth-child(9){
+                    background: darken($bg, 4*2%);
+                }
+                &:nth-child(4), &:nth-child(7), &:nth-child(10){
+                    background: darken($bg, 4*3%);
+                }
+                &:nth-child(8), &:nth-child(11), &:nth-child(13){
+                    background: darken($bg, 4*4%);
+                }
+                &:nth-child(14){
+                    background: darken($bg, 4*5%);
+                }
+                &:nth-child(12){
+                    background: darken($bg, 4*6%);
                 }
             }
         }
