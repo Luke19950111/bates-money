20e1713d3f0f73dce5c45ab23303945d6a31a823
liukuai <937613925@qq.com>
2020-07-17 17:50:56 +0800

    用 ts 实现 Types 组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -7,23 +7,20 @@
     </div>
 </template>
 
-<script lang="js">
-    export default {
-        name: 'Types',
-        data() {
-            return {
-                type: '-' //'-'表示支出，'+'表示收入
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
+
+    @Component
+    export default class Types extends Vue {
+        type = '-'; //'-'表示支出，'+'表示收入
+        selectType(type: string) {
+            if (type !== '-' && type !== '+') {
+                throw new Error('type is unknow');
             }
-        },
-        methods: {
-            selectType(type) {
-                if (type !== '-' && type !== '+') {
-                    throw new Error('type is unknow')
-                }
-                this.type = type
-            }
+            this.type = type;
         }
-    };
+    }
 </script>
 
 <style lang="scss" scoped>
