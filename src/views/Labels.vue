1be730beea6f5d13afe2a0ee12fe01e65e51a151
liukuai <937613925@qq.com>
2020-07-21 11:01:25 +0800

    完成 Labels html和css

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,6 +1,26 @@
 <template>
     <Layout>
-        labels
+        <ol class="tags">
+            <li>
+                <span>衣</span>
+                <Icon name="right"/>
+            </li>
+            <li>
+                <span>食</span>
+                <Icon name="right"/>
+            </li>
+            <li>
+                <span>住</span>
+                <Icon name="right"/>
+            </li>
+            <li>
+                <span>行</span>
+                <Icon name="right"/>
+            </li>
+        </ol>
+        <div class="createTag-wrapper">
+            <button class="createTag">新建标签</button>
+        </div>
     </Layout>
 </template>
 
@@ -11,5 +31,39 @@
 </script>
 
 <style lang="scss" scoped>
+    .tags {
+        background: #ffffff;
+        font-size: 16px;
+        padding-left: 16px;
 
+        > li {
+            min-height: 44px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            border-bottom: 1px solid #e6e6e6;
+
+            svg {
+                color: #666;
+                margin-right: 16px;
+                width: 16px;
+                height: 16px;
+            }
+        }
+    }
+
+    .createTag {
+        background: #767676;
+        color: #ffffff;
+        border-radius: 4px;
+        border: none;
+        height: 40px;
+        padding: 0 16px;
+
+        &-wrapper {
+            text-align: center;
+            padding: 16px;
+            margin-top: 44-16px;
+        }
+    }
 </style>
