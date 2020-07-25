9443040dd5713dd6fbc4a1f76e06074a5b43af3d
liukuai <937613925@qq.com>
2020-07-25 15:39:33 +0800

    用列表展示record数据；完成列表的css

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,6 +2,18 @@
     <Layout>
         <Tabs :data-source="recordTypeList" :value.sync="type" class-prefix="type"/>
         <Tabs :data-source="intervalList" :value.sync="interval" class-prefix="interval"/>
+        <ol>
+            <li v-for="(group, index) in result" :key="index">
+                <h3 class="title">{{group.title}}</h3>
+                <ol>
+                    <li v-for="item in group.items" :key="item.id" class="record">
+                        <span>{{tagString(item.tags)}}</span>
+                        <span class="notes">{{item.notes}}</span>
+                        <span>￥{{item.amount}}</span>
+                    </li>
+                </ol>
+            </li>
+        </ol>
     </Layout>
 </template>
 
@@ -16,10 +28,34 @@
         components: {Tabs}
     })
     export default class Statistics extends Vue {
+        get recordList() {
+            return (this.$store.state as RootState).recordList;
+        }
+
+        get result() {
+            const {recordList} = this;
+            type HashTableValue = { title: string; items: RecordItem[] }
+            const hashTable: { [key: string]: HashTableValue } = {};
+            for (let i = 0; i < recordList.length; i++) {
+                const [date, time] = recordList[i].createdAt!.split('T');
+                hashTable[date] = hashTable[date] || {title: date, items: []};
+                hashTable[date].items.push(recordList[i]);
+            }
+            return hashTable;
+        }
+
+        beforeCreate() {
+            this.$store.commit('fetchRecords');
+        }
+
         type = '-';
         intervalList = intervalList;
         interval = 'day';
         recordTypeList = recordTypeList;
+
+        tagString(tags: Tag[]) {
+            return tags.length === 0 ? '无' : tags.join(',');
+        }
     }
 </script>
 
@@ -38,5 +74,25 @@
 
     ::v-deep .interval-tabs-item {
         height: 48px;
+    }
+
+    %item{
+        padding: 8px 16px;
+        line-height: 24px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .title{
+        @extend %item;
+    }
+    .record{
+        background: white;
+        @extend %item;
+    }
+    .notes{
+        margin-right: auto;
+        margin-left: 16px;
+        color: #999999;
     }
 </style>
