57c93a136147e44a474087f1f66a190ba65a4738
liukuai <937613925@qq.com>
2020-07-27 14:41:58 +0800

    createTagError

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,6 +9,7 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
     state: {
         recordList: [],
+        createTagError: null,
         tagList: [],
         currentTag: undefined
     } as RootState,
@@ -36,9 +37,10 @@ const store = new Vuex.Store({
             }
         },
         createTag(state, name: string) {
+            state.createTagError = null;
             const names = state.tagList.map(item => item.name);
             if (names.indexOf(name) >= 0) {
-                window.alert('标签名重复了');
+                state.createTagError = new Error('tag name duplicated');
                 return;
             }
             const id = createId().toString();
